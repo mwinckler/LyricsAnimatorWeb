d8dd9b3713f7775ff804dc37c9d3fa63994ecab4
Matt Winckler <himself@mattwinckler.com>
2020-06-02 20:29:08 -0700

    Implement basic user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  private
+
+  def current_user
+    return @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
